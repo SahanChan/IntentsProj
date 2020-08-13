c2ab482b7ba5c2464ead5627bd7657831545defd
sahan <xnscb8.chandrabahu@gmail.com>
2020-08-13 17:43:33 +0530

    upto_8

--- app/src/main/java/com/example/intentsproj/FirstActivity.java
+++ app/src/main/java/com/example/intentsproj/FirstActivity.java
@@ -2,29 +2,43 @@ package com.example.intentsproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.icu.lang.UCharacter;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
     Button button;
+    EditText number1,number2;
+    String n1,n2;
+    String toastMessage = "You just clicked the OK button";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
         button = (Button)findViewById(R.id.button);
+        number1 = findViewById(R.id.no1);
+        number2 = findViewById(R.id.no2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openActivity2();
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                n1 = number1.getText().toString();
+                n2 = number2.getText().toString();
+                intent.putExtra("number1", n1);
+                intent.putExtra("number2", n2);
+                Toast toast = Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.TOP,Gravity.CENTER,200);
+                toast.show();
+                startActivity(intent);
+                finish();
             }
         });
-    }
-
-    public void openActivity2(){
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/intentsproj/SecondActivity.java
+++ app/src/main/java/com/example/intentsproj/SecondActivity.java
@@ -3,12 +3,98 @@ package com.example.intentsproj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
+    TextView result;
+    EditText number1,number2;
+    String n1,n2;
+    Button plus,minus,multiply,divide;
+    double ans;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        getSupportActionBar().setTitle("Second Activity");
+        number1 = findViewById(R.id.n1_2);
+        number2 = findViewById(R.id.n2_2);
+        result = findViewById(R.id.result);
+        plus = (Button) findViewById(R.id.plus);
+        minus = (Button) findViewById(R.id.minus);
+        multiply = (Button) findViewById(R.id.multiply);
+        divide = (Button) findViewById(R.id.divide);
+        n1 = getIntent().getExtras().getString("number1");
+        n2 = getIntent().getExtras().getString("number2");
+        number1.setText(n1);
+        number2.setText(n2);
+
+
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                n1 = number1.getText().toString();
+                n2 = number2.getText().toString();
+
+                ans = Double.parseDouble(n1) + Double.parseDouble(n2);
+                String answer = n1 + " + " + n2 + " = " + Double.toString(ans);
+                result.setText(answer);
+
+
+            }
+        });
+
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                n1 = number1.getText().toString();
+                n2 = number2.getText().toString();
+
+                ans =  Double.parseDouble(n1) -  Double.parseDouble(n2);
+                String answer = n1 + " - " + n2 + " = " + Double.toString(ans);
+                result.setText(answer);
+
+            }
+        });
+
+        multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                n1 = number1.getText().toString();
+                n2 = number2.getText().toString();
+
+                ans =  Double.parseDouble(n1) *  Double.parseDouble(n2);
+                String answer = n1 + " X " + n2 + " = " + Double.toString(ans);
+                result.setText(answer);
+
+
+            }
+        });
+
+        divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                n1 = number1.getText().toString();
+                n2 = number2.getText().toString();
+
+                ans =  Double.parseDouble(n1) /  Double.parseDouble(n2);
+                String answer = n1 + " / " + n2 + " = " + Double.toString(ans);
+                result.setText(answer);
+
+            }
+        });
+
+
+
+
     }
+
+
+
 }
