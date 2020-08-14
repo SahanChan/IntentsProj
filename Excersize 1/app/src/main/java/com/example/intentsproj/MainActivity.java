5bcfd54b1684d3f355146ef877c5998c876afeef
sahan <xnscb8.chandrabahu@gmail.com>
2020-08-14 07:41:24 +0530

    Excersize1

--- Excersize 1/app/src/main/java/com/example/intentsproj/MainActivity.java
+++ Excersize 1/app/src/main/java/com/example/intentsproj/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.intentsproj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
